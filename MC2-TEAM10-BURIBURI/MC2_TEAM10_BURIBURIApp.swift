bb75f0c42aa6b9c5ce59eeacc64ccdc79aebcb6c
GaOn <xnoag@icloud.com>
2023-05-10 21:16:43 +0900

    Initial Settings

---
+++ MC2-TEAM10-BURIBURI/MC2_TEAM10_BURIBURIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_TEAM10_BURIBURIApp.swift
+//  MC2-TEAM10-BURIBURI
+//
+//  Created by xnoag on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct MC2_TEAM10_BURIBURIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
