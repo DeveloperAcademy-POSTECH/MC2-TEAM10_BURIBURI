b0cdc69af45b5f6f748cba1efe42f234a1f9e46e
Oliver Wonil <stellaria700@gmail.com>
2023-05-17 12:05:55 +0900

    Feat: #1 ARView에서 ScanView로 가는 버튼 작동하도록 NavigationView로 감쌌음

--- MC2-TEAM10-BURIBURI/View/ARView.swift
+++ MC2-TEAM10-BURIBURI/View/ARView.swift
@@ -15,61 +15,63 @@ struct ARView: View {
     @StateObject var arViewState = ARViewState()
     
     var body: some View {
-        ZStack {
-            ARViewContainer().environmentObject(arViewState)
-                .ignoresSafeArea(.all)
-//                .onReceive(dataModel.$items, perform: { _ in
-//                    self.arViewState.scnNodeArray = []
-//                })
-            VStack {
-                Spacer()
-                    .frame(height: getHeight() * 0.03)
-                HStack {
-                    Spacer()
-                        .frame(width: getWidth() * 0.05, height: getHeight() * 0.2)
-                    NavigationLink(destination: CameraView()) {
-                        Image("Group")
-                            .resizable()
-                            .aspectRatio(1, contentMode: .fit)
-                            .foregroundColor(.clear)
-                            .frame(width: getWidth() * 0.15, height: getHeight() * 0.2)
-                    }
-                    Spacer()
-                        .frame(width: getWidth() * 0.8, height: getHeight() * 0.2)
-                }
-                Spacer()
-                    .frame(width: getWidth() * 1, height: getHeight() * 0.62)
-                Button(action: {
-                    let generator = UINotificationFeedbackGenerator()
-                                    generator.notificationOccurred(.success)
-                    isUnBoxing.toggle()
-                }) {
-                    if isUnBoxing {
-                        Rectangle()
-                            .aspectRatio(1, contentMode: .fit)
-                            .foregroundColor(.clear)
-                            .frame(width: getWidth() * 1, height: getHeight() * 0.12)
-                    } else {
-                        Image("C_Fedora3_02")
-                            .resizable()
-//                        Rectangle()
-                            .aspectRatio(1, contentMode: .fit)
-                            .foregroundColor(.blue)
-                            .frame(width: getWidth() * 1, height: getHeight() * 0.1)
-                    }
-                }
-                Spacer()
-                    .frame(width: getWidth() * 1, height: getHeight() * 0.05)
-            }
-            .sheet(isPresented: $isUnBoxing) {
-                NavigationView {
-                    AlbumForChildView()
-                        .environmentObject(arViewState)
-                }
-                .clearModalBackground()
-                .presentationDetents([.height(getHeight() * 0.33)])
-            }
-        }
+		NavigationView {
+			ZStack {
+				ARViewContainer().environmentObject(arViewState)
+					.ignoresSafeArea(.all)
+				//                .onReceive(dataModel.$items, perform: { _ in
+				//                    self.arViewState.scnNodeArray = []
+				//                })
+				VStack {
+					Spacer()
+						.frame(height: getHeight() * 0.03)
+					HStack {
+						Spacer()
+							.frame(width: getWidth() * 0.05, height: getHeight() * 0.2)
+						NavigationLink(destination: CameraView()) {
+							Image("Group")
+								.resizable()
+								.aspectRatio(1, contentMode: .fit)
+								.foregroundColor(.clear)
+								.frame(width: getWidth() * 0.15, height: getHeight() * 0.2)
+						}
+						Spacer()
+							.frame(width: getWidth() * 0.8, height: getHeight() * 0.2)
+					}
+					Spacer()
+						.frame(width: getWidth() * 1, height: getHeight() * 0.62)
+					Button(action: {
+						let generator = UINotificationFeedbackGenerator()
+						generator.notificationOccurred(.success)
+						isUnBoxing.toggle()
+					}) {
+						if isUnBoxing {
+							Rectangle()
+								.aspectRatio(1, contentMode: .fit)
+								.foregroundColor(.clear)
+								.frame(width: getWidth() * 1, height: getHeight() * 0.12)
+						} else {
+							Image("C_Fedora3_02")
+								.resizable()
+							//                        Rectangle()
+								.aspectRatio(1, contentMode: .fit)
+								.foregroundColor(.blue)
+								.frame(width: getWidth() * 1, height: getHeight() * 0.1)
+						}
+					}
+					Spacer()
+						.frame(width: getWidth() * 1, height: getHeight() * 0.05)
+				}
+				.sheet(isPresented: $isUnBoxing) {
+					NavigationView {
+						AlbumForChildView()
+							.environmentObject(arViewState)
+					}
+					.clearModalBackground()
+					.presentationDetents([.height(getHeight() * 0.33)])
+				}
+			}
+		}
         .navigationBarHidden(true)
     }
         
